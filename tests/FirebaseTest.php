93e0f419354f9885b2ddcc619f4e919cfa435d07
Jérôme Gamez <jerome@gamez.name>
2015-01-13 22:39:01 +0100

    Add tests for Firebase::get/set/push

--- tests/FirebaseTest.php
+++ tests/FirebaseTest.php
@@ -8,32 +8,140 @@
 
 namespace Kreait\Firebase;
 
-use Ivory\HttpAdapter\CurlHttpAdapter;
-use Prophecy\PhpUnit\ProphecyTestCase;
+use Ivory\HttpAdapter\Configuration;
+use Ivory\HttpAdapter\FopenHttpAdapter;
+use Ivory\HttpAdapter\HttpAdapterInterface;
+use Ivory\HttpAdapter\Message\Response;
+use Ivory\HttpAdapter\Message\Stream\StringStream;
 
-class FirebaseTest extends ProphecyTestCase
+class FirebaseTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * @var Firebase
      */
-    protected $f;
+    protected $instance;
+
+    /**
+     * @var string
+     */
+    protected $baseUrl = 'https://brilliant-torch-1474.firebaseio.com';
 
     protected function setUp()
     {
         parent::setUp();
 
-        $this->f = new Firebase('https://example.com');
+        $this->instance = new Firebase($this->baseUrl);
     }
 
     public function testDefaultState()
     {
-        $this->assertAttributeInstanceOf('Ivory\HttpAdapter\CurlHttpAdapter', 'http', $this->f);
+        $this->assertAttributeInstanceOf('Ivory\HttpAdapter\CurlHttpAdapter', 'http', $this->instance);
+        $this->assertInstanceOf('Psr\Log\NullLogger', $this->instance->getLogger());
+        $this->assertEquals($this->baseUrl, $this->instance->getBaseUrl());
     }
 
-    public function testInitialState()
+    public function testInitializeFirebaseWithCustomHttpAdapter()
     {
-        $f = new Firebase('https://example.com', $http = new CurlHttpAdapter());
+        $f = new Firebase($this->baseUrl, $http = new FopenHttpAdapter());
 
         $this->assertAttributeSame($http, 'http', $f);
+    }
+
+    public function testSetLogger()
+    {
+        $this->instance->setLogger($logger = $this->getMock('Psr\Log\LoggerInterface'));
+        $this->assertAttributeSame($logger, 'logger', $this->instance);
+        $this->assertSame($logger, $this->instance->getLogger());
+    }
+
+    public function testSet()
+    {
+        $data = [
+            'key' => 'value2',
+            'key2' => 'value2'
+        ];
+
+        $http = $this->getMockHttpAdapter();
+        $http->method('sendRequest')->willReturn($this->getJsonResponse($data));
+
+        $f = new Firebase($this->baseUrl, $http);
+
+        $result = $f->set($data, 'test/' . __METHOD__);
+
+        $this->assertEquals($data, $result);
+    }
+
+    public function testPush()
+    {
+        $data = [
+            'key' => 'value2',
+            'key2' => 'value2'
+        ];
+
+        $http = $this->getMockHttpAdapter();
+        $http->method('sendRequest')->willReturn($this->getJsonResponse(['name' => 'foo']));
+
+        $f = new Firebase($this->baseUrl, $http);
+
+        $result = $f->push($data, 'test/' . __METHOD__);
+
+        $this->assertEquals('foo', $result);
+    }
+
+    public function testGet()
+    {
+        $data = [
+            'key' => 'value2',
+            'key2' => 'value2'
+        ];
+
+        $http = $this->getMockHttpAdapter();
+        $http->method('sendRequest')->willReturn($this->getJsonResponse($data));
+
+        $f = new Firebase($this->baseUrl, $http);
+
+        $result = $f->get('test/' . __METHOD__);
+
+        $this->assertEquals($data, $result);
+    }
+
+    /**
+     * @covers Kreait\Firebase\Firebase::createRequestParams
+     * @covers Kreait\Firebase\Firebase::send
+     */
+    public function testGetWithShallowOption()
+    {
+        $data = [
+            'key' => 'value2',
+            'key2' => 'value2'
+        ];
+
+        $http = $this->getMockHttpAdapter();
+        $http->method('sendRequest')->willReturn($this->getJsonResponse($data));
+
+        $f = new Firebase($this->baseUrl, $http);
+
+        $result = $f->get('test/' . __METHOD__, ['shallow' => true]);
+
+        $this->assertEquals($data, $result);
+    }
+
+    /**
+     * @return HttpAdapterInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getMockHttpAdapter()
+    {
+        $http = $this->getMockBuilder('Ivory\HttpAdapter\HttpAdapterInterface')
+            ->getMock();
+
+        $http->method('getConfiguration')
+            ->willReturn(new Configuration());
+
+        return $http;
+    }
+
+    protected function getJsonResponse(array $data)
+    {
+        return new Response(200, 'OK', Response::PROTOCOL_VERSION_1_1, [], new StringStream(json_encode($data)));
     }
 }
