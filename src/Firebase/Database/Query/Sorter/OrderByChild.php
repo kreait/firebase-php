1ab0c907797348c6655890edcce2cebd497664d6
Johan Alvarez <llstarscreamll@hotmail.com>
2017-11-24 09:44:13 -0500

    prevent throw nested array error on orderByChild('foo/bar')

--- src/Firebase/Database/Query/Sorter/OrderByChild.php
+++ src/Firebase/Database/Query/Sorter/OrderByChild.php
@@ -27,13 +27,58 @@ final class OrderByChild implements Sorter
         if (!is_array($value)) {
             return $value;
         }
-
+        
         $childKey = $this->childKey;
-
+        
         uasort($value, function ($a, $b) use ($childKey) {
-            return ($a[$childKey] ?? null) <=> $b[$childKey] ?? null;
+            return ($this->get($a, $childKey) ?? null) <=> $this->get($b, $childKey) ?? null;
         });
-
+        
         return $value;
+    }
+
+    /**
+     * Function taken from Mohamed Meabed repo:
+     * https://github.com/tajawal/lodash-php/blob/master/src/collections/get.php
+     * 
+     * Get item of an array by index , aceepting nested index
+     *
+     ** __::get(['foo' => ['bar' => 'ter']], 'foo/bar');
+     ** // → 'ter'
+     *
+     * @param array  $collection array of values
+     * @param string $key        key or index
+     * @param null   $default    default value to return if index not exist
+     *
+     * @return array|mixed|null
+     *
+     */
+    function get($collection = [], $key = '', $default = null)
+    {
+        if (is_null($key)) {
+            return $collection;
+        }
+
+        if (!is_object($collection) && isset($collection[$key])) {
+            return $collection[$key];
+        }
+
+        foreach (explode('/', $key) as $segment) {
+            if (is_object($collection)) {
+                if (!isset($collection->{$segment})) {
+                    return $default instanceof \Closure ? $default() : $default;
+                } else {
+                    $collection = $collection->{$segment};
+                }
+            } else {
+                if (!isset($collection[$segment])) {
+                    return $default instanceof \Closure ? $default() : $default;
+                } else {
+                    $collection = $collection[$segment];
+                }
+            }
+        }
+
+        return $collection;
     }
 }
