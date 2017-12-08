29f98e5b6353ec3472a09dd9b5a88df7b975035d
Jérôme Gamez <jerome@gamez.name>
2017-12-08 17:20:00 +0100

    Add failing test for ordering by nested children

--- tests/Firebase/Database/Query/Sorter/OrderByChildTest.php
+++ tests/Firebase/Database/Query/Sorter/OrderByChildTest.php
@@ -9,39 +9,34 @@ use Kreait\Tests\FirebaseTestCase;
 class OrderByChildTest extends FirebaseTestCase
 {
     /**
-     * @var OrderByChild
-     */
-    protected $sorter;
-
-    protected function setUp()
-    {
-        $this->sorter = new OrderByChild('key');
-    }
-
-    public function testModifyUri()
-    {
-        $this->assertContains(
-            'orderBy='.rawurlencode('"key"'),
-            (string) $this->sorter->modifyUri(new Uri('http://domain.tld'))
-        );
-    }
-
-    /**
+     * @param string $childKey
+     * @param mixed $expected
+     * @param mixed $given
+     *
      * @dataProvider valueProvider
      */
-    public function testModifyValue($expected, $value)
+    public function testOrderByChild($childKey, $expected, $given)
     {
-        $this->assertSame($expected, $this->sorter->modifyValue($value));
+        $sut = new OrderByChild($childKey);
+
+        $this->assertContains(
+            'orderBy='.rawurlencode(sprintf('"%s"', $childKey)),
+            (string) $sut->modifyUri(new Uri('http://domain.tld'))
+        );
+
+        $this->assertSame($expected, $sut->modifyValue($given));
     }
 
     public function valueProvider()
     {
         return [
             'scalar' => [
+                'key' => 'key',
                 'expected' => 'scalar',
                 'given' => 'scalar',
             ],
             'array' => [
+                'key' => 'key',
                 'expected' => [
                     'third' => ['key' => 1],
                     'fourth' => ['key' => 2],
@@ -55,6 +50,37 @@ class OrderByChildTest extends FirebaseTestCase
                     'fourth' => ['key' => 2],
                 ],
             ],
+            'nested' => [
+                'key' => 'child/grandchild',
+                'expected' => [
+                    'third' => ['child' => ['grandchild' => 1]],
+                    'fourth' => ['child' => ['grandchild' => 2]],
+                    'first'  => ['child' => ['grandchild' => 3]],
+                    'second' => ['child' => ['grandchild' => 4]],
+                ],
+                'given' => [
+                    'first'  => ['child' => ['grandchild' => 3]],
+                    'second' => ['child' => ['grandchild' => 4]],
+                    'third' => ['child' => ['grandchild' => 1]],
+                    'fourth' => ['child' => ['grandchild' => 2]],
+                ],
+            ],
+            'super_nested' => [
+                'key' => 'child/grandchild/great_grandchild',
+                'expected' => [
+                    'third' => ['child' => ['grandchild' => ['great_grandchild' => 1]]],
+                    'fourth' => ['child' => ['grandchild' => ['great_grandchild' => 2]]],
+                    'first'  => ['child' => ['grandchild' => ['great_grandchild' => 3]]],
+                    'second' => ['child' => ['grandchild' => ['great_grandchild' => 4]]],
+                ],
+                'given' => [
+                    'first'  => ['child' => ['grandchild' => ['great_grandchild' => 3]]],
+                    'second' => ['child' => ['grandchild' => ['great_grandchild' => 4]]],
+                    'third' => ['child' => ['grandchild' => ['great_grandchild' => 1]]],
+                    'fourth' => ['child' => ['grandchild' => ['great_grandchild' => 2]]],
+                ],
+            ],
+
         ];
     }
 }
