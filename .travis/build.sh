694383cbc1aa6704878ea2c5ae648e7d24b5bfad
Jérôme Gamez <jerome@gamez.name>
2018-03-27 02:20:13 +0200

    Extend and clean up Travis CI builds

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,9 +2,16 @@
 
 if [ "${UNIT_TESTS}" = "1" ]; then
   echo "Running unit tests"
-  vendor/bin/phpunit $PHPUNIT_FLAGS
+  vendor/bin/phpunit --testsuite unit
 else
   echo "Not running unit tests"
+fi
+
+if [ "${INTEGRATION_TESTS}" = "1" ]; then
+  echo "Running integration tests"
+  vendor/bin/phpunit --testsuite integration
+else
+  echo "Not running integration tests"
 fi
 
 if [ "${STATIC_ANALYSIS}" = "1" ]; then
--- .travis/install.sh
+++ .travis/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -ev
 
-composer update $COMPOSER_FLAGS $DEFAULT_COMPOSER_FLAGS
+composer update $COMPOSER_FLAGS --profile --ansi --prefer-dist --no-interaction --optimize-autoloader --no-suggest --no-progress
