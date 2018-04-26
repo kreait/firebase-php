f33745ab3c6ba5b84327f26c50b8746c09a7ec6a
Casper Rasmussen <cr@nodes.dk>
2018-04-26 16:19:11 +0000

    Added messaging configs for android, apns & web

---
+++ src/Firebase/Messaging/AndroidConfig.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kreait\Firebase\Messaging;
+
+use Kreait\Firebase\Exception\InvalidArgumentException;
+
+class AndroidConfig implements Config
+{
+    /**
+     * @var array
+     */
+    private $data;
+
+
+    private function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    public static function fromArray(array $data): self
+    {
+
+        try {
+            return new self($data);
+        } catch (\Throwable $e) {
+            throw new InvalidArgumentException($e->getMessage(), $e->getCode(), $e);
+        }
+    }
+
+    public function setData(array $data): self
+    {
+        $config = clone $config;
+        $config->data = $data;
+
+        return $config;
+    }
+
+    /**
+     * @return array
+     */
+    public function data(): array
+    {
+        return $this->data;
+    }
+
+    public function jsonSerialize()
+    {
+        return array_filter($this->data, function ($value) {
+            return null !== $value;
+        });
+    }
+}
