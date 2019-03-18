ec5ed0ff248260f8903f41759e5bb703578e6c98
Jérôme Gamez <jerome@gamez.name>
2019-03-18 21:50:04 +0100

    Regenerate test device tokens

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -5,7 +5,7 @@ if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
   echo "Decrypting test credentials"
   openssl aes-256-cbc -K $encrypted_8bfcfbcad9c4_key -iv $encrypted_8bfcfbcad9c4_iv -in test_credentials.json.enc -out tests/_fixtures/test_credentials.json -d;
   echo "Decrypting test devices"
-  openssl aes-256-cbc -K $encrypted_9b0dc8718c27_key -iv $encrypted_9b0dc8718c27_iv -in test_devices.json.enc -out tests/_fixtures/test_devices.json -d;
+  openssl aes-256-cbc -K $encrypted_d51fa485ef84_key -iv $encrypted_d51fa485ef84_iv -in test_devices.json.enc -out tests/_fixtures/test_devices.json -d;
 else
   echo "Not decrypting test credentials or test devices"
 fi
