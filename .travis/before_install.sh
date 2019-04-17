2d0f523a00fad7951b1201294e2b2e2c7a0cec51
Jérôme Gamez <jerome@gamez.name>
2019-04-17 09:42:05 +0200

    Recreate encrypted secrets

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -2,11 +2,10 @@
 set -evx
 
 if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
-  echo "Decrypting test credentials"
-  openssl aes-256-cbc -K $encrypted_8bfcfbcad9c4_key -iv $encrypted_8bfcfbcad9c4_iv -in test_credentials.json.enc -out tests/_fixtures/test_credentials.json -d;
-  echo "Decrypting test devices"
-  openssl aes-256-cbc -K $encrypted_d51fa485ef84_key -iv $encrypted_d51fa485ef84_iv -in test_devices.json.enc -out tests/_fixtures/test_devices.json -d;
+  echo "Decrypting secrets"
+  openssl aes-256-cbc -K $encrypted_d51fa485ef84_key -iv $encrypted_d51fa485ef84_iv -in secrets.tar.enc -out secrets.tar -d;
+  tar xvf secrets.tar
 else
-  echo "Not decrypting test credentials or test devices"
+  echo "Not decrypting secrets"
 fi
 
