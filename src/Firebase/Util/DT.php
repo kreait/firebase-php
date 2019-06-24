dae20411de86ffbe8be4b71241ccefe791fb58f0
Jérôme Gamez <jerome@gamez.name>
2019-06-24 10:56:54 +0200

    Make DT helper more resilient

--- src/Firebase/Util/DT.php
+++ src/Firebase/Util/DT.php
@@ -23,31 +23,45 @@ class DT
         $tz = new DateTimeZone('UTC');
         $now = \time();
 
-        if (\ctype_digit($value)) {
-            // Seconds
-            if (\mb_strlen((string) $value) === \mb_strlen((string) $now)) {
-                return DateTimeImmutable::createFromFormat('U', (string) $value)
-                    ->setTimezone($tz);
-            }
-
-            // Milliseconds
-            if (\mb_strlen((string) $value) === \mb_strlen((string) ($now * 1000))) {
-                return DateTimeImmutable::createFromFormat('U.u', \sprintf('%F', $value / 1000))
-                    ->setTimezone($tz);
+        if ($value instanceof \DateTimeInterface) {
+            if ($result = DateTimeImmutable::createFromFormat('U.u', $value->format('U.u'))) {
+                return $result->setTimezone($tz);
             }
         }
 
-        if ($value instanceof \DateTimeInterface) {
-            return DateTimeImmutable::createFromFormat('U.u', $value->format('U.u'))
-                ->setTimezone($tz);
+        if ($value === null || $value === 0 || \is_bool($value)) {
+            $value = '0';
+        }
+
+        try {
+            $value = (string) $value;
+        } catch (\Throwable $e) {
+            throw new InvalidArgumentException($e->getMessage());
+        }
+
+        if (\ctype_digit($value)) {
+            // Seconds
+            if ($value === '0' || \mb_strlen($value) === \mb_strlen((string) $now)) {
+                if ($result = DateTimeImmutable::createFromFormat('U', $value)) {
+                    return $result->setTimezone($tz);
+                }
+            }
+
+            // Milliseconds
+            if (\mb_strlen($value) === \mb_strlen((string) ($now * 1000))) {
+                if ($result = DateTimeImmutable::createFromFormat('U.u', \sprintf('%F', (float) ($value / 1000)))) {
+                    return $result->setTimezone($tz);
+                }
+            }
         }
 
         // microtime
         if (\preg_match('@(?P<msec>^0?\.\d+) (?P<sec>\d+)$@', $value, $matches)) {
             $value = (float) $matches['sec'] + (float) $matches['msec'];
 
-            return DateTimeImmutable::createFromFormat('U.u', \sprintf('%F', $value))
-                ->setTimezone($tz);
+            if ($result = DateTimeImmutable::createFromFormat('U.u', \sprintf('%F', $value))) {
+                return $result->setTimezone($tz);
+            }
         }
 
         try {
