43a843809f37bf14c1bb2e1d1feb2d1de27987c5
Somogyi László <slaci@webtown.hu>
2019-09-18 20:29:44 +0200

    Introduce constants for Identity Provider IDs

--- src/Firebase/Value/Provider.php
+++ src/Firebase/Value/Provider.php
@@ -6,11 +6,18 @@ namespace Kreait\Firebase\Value;
 
 use Kreait\Firebase\Value;
 
-/**
- * @internal
- */
 class Provider implements \JsonSerializable, Value
 {
+    const ANONYMOUS = 'anonymous';
+    const CUSTOM = 'custom';
+    const FACEBOOK = 'facebook.com';
+    const FIREBASE = 'firebase';
+    const GITHUB = 'github.com';
+    const GOOGLE = 'google.com';
+    const PASSWORD = 'password';
+    const PHONE = 'phone';
+    const TWITTER = 'twitter.com';
+
     /**
      * @var string
      */
