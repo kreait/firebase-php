e7587a89a4847a4d1185eacf20ac0df75822a9ce
Petr Arapov <petr@cybrary.it>
2021-07-19 08:22:58 -0400

    fix SpTest

--- tests/Unit/IdentityPlatform/SpConfigTest.php
+++ tests/Unit/IdentityPlatform/SpConfigTest.php
@@ -6,21 +6,24 @@ namespace Kreait\Firebase\Tests\Unit\IdentityPlatform;
 
 use Kreait\Firebase\Tests\UnitTestCase;
 use Kreait\Firebase\IdentityPlatform\SpConfig;
+use Kreait\Firebase\Value\Url;
 
 class SpConfigTest extends UnitTestCase
 {
     public function testWithProperties()
     {
         $properties = [
-
-
                 'spEntityId' => 'testSp',
                 'callbackUri' => 'https://google.com',
+                'spCertificates' => []
         ];
+
+        $propertiesConverted = $properties;
+        $propertiesConverted['callbackUri'] = Url::fromValue($properties['callbackUri']);
 
         $instance = SpConfig::withProperties($properties);
 
-        $this->assertEquals($properties, $instance->toArray());
-        $this->assertEquals($properties, $instance->jsonSerialize());
+        $this->assertEquals($propertiesConverted, $instance->toArray());
+        $this->assertEquals($propertiesConverted, $instance->jsonSerialize());
     }
 }
