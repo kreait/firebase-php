10d3995ba6fdae01222e6250e8e6340b86847eb2
Jérôme Gamez <jerome@gamez.name>
2022-08-16 23:45:49 +0200

    Fix limits and filters not being applied to Realtime Database Queries

---
+++ tests/Integration/Database/QueryTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kreait\Firebase\Tests\Integration\Database;
+
+use Kreait\Firebase\Contract\Database;
+use Kreait\Firebase\Database\Reference;
+use Kreait\Firebase\Database\RuleSet;
+use Kreait\Firebase\Tests\Integration\DatabaseTestCase;
+
+/**
+ * @internal
+ * @group database-emulator
+ * @group emulator
+ */
+final class QueryTest extends DatabaseTestCase
+{
+    private Reference $ref;
+
+    protected function setUp(): void
+    {
+        $this->ref = self::$db->getReference(self::$refPrefix);
+    }
+
+    public function testLimitToLast(): void
+    {
+        $ref = $this->ref->getChild(__FUNCTION__);
+
+        $rules = self::$db->getRuleSet()->getRules();
+
+        $rules['rules'][$this->ref->getPath()] =
+            [__FUNCTION__ => ['.indexOn' => ['key']]
+        ];
+
+        self::$db->updateRules(RuleSet::fromArray($rules));
+
+        $ref->push(['key' => 1]);
+        $ref->push(['key' => 3]);
+        $ref->push(['key' => 2]);
+
+        $value = $ref->orderByChild('key')->limitToLast(1)->getValue();
+
+        $this->assertSame(['key' => 3], current($value));
+    }
+}
