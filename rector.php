295595ad2a2b8b19ef9721ca418d5f27db38da43
Jérôme Gamez <jerome@gamez.name>
2023-06-08 23:16:57 +0200

    Update Rector

--- rector.php
+++ rector.php
@@ -5,6 +5,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 
@@ -13,6 +14,8 @@ return static function (RectorConfig $rectorConfig): void {
         __DIR__.'/src',
         __DIR__.'/tests',
     ]);
+
+    $rectorConfig->cacheDirectory(__DIR__.'/.build/rector');
 
     // $rectorConfig->importNames();
 
@@ -23,5 +26,9 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([
         SetList::EARLY_RETURN,
         LevelSetList::UP_TO_PHP_81,
+    ]);
+
+    $rectorConfig->skip([
+        AddLiteralSeparatorToNumberRector::class,
     ]);
 };
