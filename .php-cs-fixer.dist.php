cf81cc8edc2ad516cda1b974c14a5688d9c31853
Jérôme Gamez <jerome@gamez.name>
2023-10-09 10:58:18 +0200

    Update PHP CS Fixer config to use PER CS 2.0 with modifications

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,35 +2,26 @@
 
 declare(strict_types=1);
 
-use Beste\PhpCsFixer\Config\Factory;
-use Beste\PhpCsFixer\Config\RuleSet\Php81;
+$finder = PhpCsFixer\Finder::create()->in(__DIR__);
 
-$config = Factory::fromRuleSet(new Php81(), [
-    'final_class' => false,
-    'final_internal_class' => false,
-    'global_namespace_import' => [
-        'import_classes' => true,
-        'import_constants' => true,
-        'import_functions' => true,
-    ],
-    'php_unit_method_casing' => [
-        'case' => 'camel_case',
-    ],
-    'php_unit_test_case_static_method_calls' => [
-        'call_type' => 'this',
-        'methods' => [],
-    ],
-    'php_unit_test_class_requires_covers' => false,
-    'yoda_style' => false,
-]);
-
-$config
-    ->getFinder()
-    ->in([
-        'src',
-        'tests',
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PER-CS2.0' => true,
+        'class_definition' => [
+            'single_line' => true,
+        ],
+        'concat_space' => [
+            'spacing' => 'none',
+        ],
+        'php_unit_method_casing' => [
+            'case' => 'camel_case',
+        ],
+        'php_unit_test_case_static_method_calls' => [
+            'call_type' => 'this',
+            'methods' => [],
+        ],
+        'single_line_empty_body' => false,
+        'yoda_style' => false,
     ])
-    ->ignoreDotFiles(false)
-;
-
-return $config;
+    ->setFinder($finder);
