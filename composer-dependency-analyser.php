073b3f20f129634d424803ba4ccbdfafcd777366
Jan Nedbal <janedbal@gmail.com>
2024-04-11 12:40:16 +0200

    composer-dependency-analyser: update to 1.5.0 (support function detection)

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -9,7 +9,5 @@ return (new Configuration())
     ->addPathToScan(__FILE__, true)
     ->addPathToScan(__DIR__.'/src', false)
     ->addPathToScan(__DIR__.'/tests', true)
-    // Functions are currently not detected: https://github.com/shipmonk-rnd/composer-dependency-analyser/issues/67
-    ->ignoreErrorsOnPackage('mtdowling/jmespath.php', [ErrorType::UNUSED_DEPENDENCY])
     ->ignoreErrorsOnPackage('google/cloud-firestore', [ErrorType::DEV_DEPENDENCY_IN_PROD])
 ;
