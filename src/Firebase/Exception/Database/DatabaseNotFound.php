f58320a12bbd03d59ec820a87c4242f8d9796348
Jérôme Gamez <jerome@gamez.name>
2024-04-25 12:35:51 +0200

    Update URLs

--- src/Firebase/Exception/Database/DatabaseNotFound.php
+++ src/Firebase/Exception/Database/DatabaseNotFound.php
@@ -46,7 +46,7 @@ final class DatabaseNotFound extends LogicException implements DatabaseException
             suffix.
 
             For instructions on how to set the name of the used Realtime Database, please
-            see https://firebase-php.readthedocs.io/en/6.x/#quick-start
+            see https://firebase-php.readthedocs.io/en/latest/#quick-start
 
 
             MESSAGE;
