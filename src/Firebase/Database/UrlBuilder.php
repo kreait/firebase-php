fac4eefedf17f759f977ac80306340d743cc3c14
Jérôme Gamez <jerome@gamez.name>
2024-08-19 01:05:54 +0200

    Migrate the Rector config

--- src/Firebase/Database/UrlBuilder.php
+++ src/Firebase/Database/UrlBuilder.php
@@ -7,7 +7,6 @@ namespace Kreait\Firebase\Database;
 use Kreait\Firebase\Exception\InvalidArgumentException;
 use Kreait\Firebase\Util;
 
-use function assert;
 use function http_build_query;
 use function in_array;
 use function preg_match;
