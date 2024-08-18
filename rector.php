bc35afd8fa0cc04bbf9c3dd64dd757d0039f8ac8
Jérôme Gamez <jerome@gamez.name>
2024-08-19 01:05:54 +0200

    Migrate the Rector config

--- rector.php
+++ rector.php
@@ -5,32 +5,15 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector;
-use Rector\PHPUnit\Set\PHPUnitSetList;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__.'/src',
         __DIR__.'/tests',
-    ]);
-
-    $rectorConfig->cacheDirectory(__DIR__.'/.build/rector');
-
-    $rectorConfig->importNames();
-
-    // register a single rule
-    // $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
-
-    // define sets of rules
-    $rectorConfig->sets([
-        SetList::EARLY_RETURN,
-        LevelSetList::UP_TO_PHP_81,
-        PHPUnitSetList::PHPUNIT_100,
-    ]);
-
-    $rectorConfig->skip([
-        AddLiteralSeparatorToNumberRector::class,
-    ]);
-};
+    ])
+    ->withPhpSets()
+    ->withPreparedSets(
+        earlyReturn: true,
+        phpunit: true,
+    )
+;
